0e17942d9b68a61c7baa2807c1a3e9dd5e9df392
Javier Calatrava Llavería <javi.calatrava@gmail.com>
2015-01-10 09:14:05 +0100

    Basic sprite image animation

---
+++ watchkitImageAnimation/watchkitImageAnimation WatchKit Extension/InterfaceController.m
@@ -0,0 +1,48 @@
+//
+//  InterfaceController.m
+//  watchkitImageAnimation WatchKit Extension
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 10/01/15.
+//  Copyright (c) 2015 JAVIER CALATRAVA LLAVERIA. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *imgSpriteAnimation;
+
+@end
+
+
+@implementation InterfaceController
+
+- (instancetype)initWithContext:(id)context {
+    self = [super initWithContext:context];
+    if (self){
+        // Initialize variables here.
+        // Configure interface objects here.
+        NSLog(@"%@ initWithContext", self);
+        
+    }
+    return self;
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    
+    [self.imgSpriteAnimation setImageNamed:@"dragon-"];
+    [self.imgSpriteAnimation startAnimatingWithImagesInRange:NSMakeRange(0, 60) duration:1.0 repeatCount:0];
+    
+    NSLog(@"%@ will activate", self);
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    NSLog(@"%@ did deactivate", self);
+}
+
+@end
+
+
+
